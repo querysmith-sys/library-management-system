476801a91f68ec6b24e5bd679efd04f26a658ed2
querysmith-sys <dipokgithub@gmail.com>
2026-01-19 14:05:37 +0600

    added:clerks-APIS

--- FE/src/components/pages/clerk.tsx
+++ FE/src/components/pages/clerk.tsx
@@ -1,10 +1,9 @@
-import { Booktable } from "../booktable";
+
 import AdminLayout from "../adminLayout";
 function ClerkManagementPage() {
      return (
         <AdminLayout> 
             <h1>Clerk Management Page</h1>
-            <Booktable/>
         </AdminLayout>
      )
 }
